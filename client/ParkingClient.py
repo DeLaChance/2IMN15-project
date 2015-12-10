4b01dc373230789052354cf91906d2c2e379a328
Lucien <ldjvalstar@gmail.com>
2015-12-10 13:32:53 +0000

    Begin gemaakt met sensueren en actueren van Pi

---
+++ client/ParkingClient.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+import Sensing as sens;
+
+def main():
+	sens.initSensing();
+	
+
+if __name__ == "__main__": 
+    main();
--- client/Sensing.py
+++ client/Sensing.py
@@ -1,1 +1,14 @@
- 
+#!/usr/bin/python
+from sense_hat import SenseHat
+
+from pisense import SenseStick
+
+sense = None;
+
+def initSensing():
+	sense = SenseHat();
+	sense.clear();
+	#sense.show_message("Parking spot on!");
+
+	stick = SenseStick()
+
