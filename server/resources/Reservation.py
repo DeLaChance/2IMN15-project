02caf4e9d1e484d094c1d31d85bfce6cc26775b3
washizzle <mauriceho@hotmail.com>
2016-01-07 16:10:29 +0100

    post werkt, delete nog niet, lijkt erop dat hij niks met de payload doet die hij mee krijgt

--- server/resources/Reservation.py
+++ server/resources/Reservation.py
@@ -1,17 +1,20 @@
+import json
+import time
 from BaseResource import BaseResource
+
 
 class Reservation(BaseResource):
     def __init__(self, name="Reservation", coap_server=None):
         super(Reservation, self).__init__(name, coap_server, visible=True,
-                                            observable=True, allow_children=True)
+                                          observable=True, allow_children=True)
 
     def render_GET(self, request):
         parkingSpotId = request.options[1].value
 
         # SQL query to retrieve reservations
-        query = "SELECT * FROM reservations"
+        query = "SELECT * FROM reservations WHERE parkingSpotId = {}".format(parkingSpotId)
         if self.index is not None:
-            query += " WHERE reservationId = {}".format(self.index)
+            query += " AND reservationId = {}".format(self.index)
 
         # Execute SQL
         rows = self._execute_SQL(query)
@@ -29,10 +32,46 @@ class Reservation(BaseResource):
         return self
 
     def render_POST(self, request):
-        res = Reservation()
-        res.location_query = request.uri_query
-        res.payload = request.payload
-        return res
+        if self.index is not None:
+            self.payload = '{"error": true, "message": "No index should be given when using POST"}'
+        else:
+            data = json.loads(request.payload)
+            if data["from"] > data["to"]:
+                self.payload = '{"error": true, "message": "The TO value should not be lower than the FROM value}'
+            else:
+                if not self._fromto_is_ok(data["parkingSpotId"], data["from"], data["to"]):
+                    self.payload = '{"error": true, "message": "There already is a reservation on this spot during this time interval"}'
+                else:
+                    query = "INSERT INTO reservations (vehicleId, parkingSpotId, 'from', 'to') values "
+                    query += "({}".format(data["vehicleId"])
+                    query += ", {}".format(data["parkingSpotId"])
+                    query += ", {}".format(data["from"])
+                    query += ", {}".format(data["to"])
+                    query += ")"
+                    self.payload = self._execute_SQL(query)
+                    print query
+        return self
+
+    def _fromto_is_ok(self, parkingspotId, fromm, to):
+        query = "SELECT * FROM reservations WHERE parkingSpotId = {}".format(parkingspotId)
+        rows = self._execute_SQL(query)
+        if len(rows) != 0:
+            for row in rows:
+                if fromm <= row["to"] and row["from"] <= to:
+                    return False
+        return True
 
     def render_DELETE(self, request):
-        return True
+        data = json.loads(request.payload)
+        state = "free"
+        fromm = data["from"]
+        to = data["to"]
+        query = "DELETE FROM reservations WHERE reservationId = {}".format(self.index)
+        query += " AND parkingSpotId = {}".format(data["parkingSpotId"])
+        current_time = time.time()
+        if fromm < current_time < to:
+            query += "; UPDATE parkingspots SET state = {}".format(state)
+            query += " WHERE parkingSpotId = {}".format(data["parkingSpotId"])
+            query += ";"
+        print self._execute_SQL(query)
+        return self
